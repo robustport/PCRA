c7aade547a373d20afa28e55591a344001bfb8b4
martinrd <martinrd@comcast.net>
2022-07-25 11:45:39 -0700

    Tiny demo changes

--- R/barplotWts.R
+++ R/barplotWts.R
@@ -1,6 +1,8 @@
 #' barplotWts
+#' 
+#' Brief description will be added here
 #'
-#' @param wts.efront 
+#' @param wts.efront Weights along the efficient frontiers
 #' @param legend.text 
 #' @param col 
 #' @param ylab 
