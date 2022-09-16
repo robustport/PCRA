48e542135e29eaef14535fa6eb93dd6d39787cae
martinrd <martinrd@comcast.net>
2022-09-16 14:24:35 -0700

    Edit to fix function.

--- R/mathEfrontCashRisky.R
+++ R/mathEfrontCashRisky.R
@@ -29,50 +29,49 @@
 #' args(mathEfrontCashRisky)
 mathEfrontCashRisky <-
 		function(returns, npoints = 10, rf = 0.005, plot.efront = TRUE,
-		    display.stocks = TRUE, stock.names = TRUE, values = FALSE,
-				scalex = 1.1, scaley = 1.1, cexPoints = 0.8, cexText = 0.8)
+		    stock.names = TRUE, values = FALSE,scalex = 1.1, scaley = 1.1,
+		    cexPoints = 0.8, cexText = 0.8)
 {
-	V = var(returns)
-	mu.stocks = apply(returns, 2, mean)
-	sigma.stocks = apply(returns, 2, sd)
-	mue = mu.stocks - rf
-	a = solve(V, mue)                  # a = Vinv * mue
-	b = as.numeric(t(mue) %*% a)       # b = muetp * Vinv* mue
-	sr.opt = sqrt(as.numeric(b))
-	bsr = sqrt(b)
-	mu.max = scaley * max(mu.stocks)
-	muvals = seq(0,mu.max, length.out = npoints)
-	sigmavals = c(muvals/bsr)
-	muvalse = seq(0,mu.max, length.out = npoints)
-	muvals = rf + muvalse
-	if(plot.efront)
-	{
-		x = sigmavals; y = muvals
-		xlim = c(0,scalex*max(sigma.stocks,x))
-		ylim = c(min(mu.stocks),max(muvals))
-		plot(x, y, type = "l", xaxs = "i", lwd = 2, xlim = xlim, ylim = ylim,
-				xlab = "Volatility", ylab = "Mean Return")
-		if(display.stocks){
-		points(sigma.stocks, mu.stocks, pch = 20, cex = cexPoints)
-		if(stock.names){
-		  text(sigma.stocks + 0.02*xlim[2], mu.stocks, names(returns),
-						cex = cexText, adj = 0)
+		  V = var(returns)
+		  mu.stocks = apply(returns, 2, mean)
+		  sigma.stocks = apply(returns, 2, sd)
+		  mue = mu.stocks - rf
+		  a = solve(V, mue)                  # a = Vinv * mue
+		  b = as.numeric(t(mue) %*% a)       # b = muetp * Vinv* mue
+		  sr.opt = sqrt(as.numeric(b))
+		  bsr = sqrt(b)
+		  mu.max = scaley * max(mu.stocks)
+		  muvals = seq(0,mu.max, length.out = npoints)
+		  sigmavals = c(muvals/bsr)
+		  muvalse = seq(0,mu.max, length.out = npoints)
+		  muvals = rf + muvalse
+		  if(plot.efront)
+		  {
+		    x = sigmavals; y = muvals
+		    xlim = c(0,scalex*max(sigma.stocks,x))
+		    ylim = c(min(mu.stocks),max(muvals))
+		    plot(x, y, type = "l", xaxs = "i", lwd = 2, xlim = xlim, ylim = ylim,
+		         xlab = "Volatility", ylab = "Mean Return")
+		    points(sigma.stocks, mu.stocks, pch = 20, cex = cexPoints)
+		    if(stock.names){
+		      text(sigma.stocks + 0.02*xlim[2], mu.stocks, names(returns),
+		           cex = cexText, adj = 0)
+		    }
+		    x <- xlim[1] + .05 * (xlim[2] - xlim[1])
+		    y <- ylim[2] - .02 * (ylim[2] - ylim[1])
+		    text(x,y,paste("SR = ",round(sr.opt,2),sep = ""),pos = 4)
+		    y = y - .07*(ylim[2] - ylim[1])
+		    text(x,y,paste("RF= ",round(rf,3),sep = ""),pos = 4)
 		  }
-		}
-		x <- xlim[1] + .05 * (xlim[2] - xlim[1])
-		y <- ylim[2] - .02 * (ylim[2] - ylim[1])
-		text(x,y,paste("SR = ",round(sr.opt,2),sep = ""),pos = 4)
-		y = y - .07*(ylim[2] - ylim[1])
-		text(x,y,paste("RF= ",round(rf,3),sep = ""),pos = 4)
-
-# Compute cash and risky assets weights
-	wts.risky = (sum(a)/b)*muvalse
-	wts.cash = 1 - wts.risky
-	wts.efront = rbind(muvals,sigmavals,wts.cash,wts.risky)
-	row.names(wts.efront) = c("MU","VOL","Cash","Risky Assets")
-
-	if(values){
-	  return(wts.efront)
-	  } 
-	}
+		  
+		  # Compute cash and risky assets weights
+		  wts.risky = (sum(a)/b)*muvalse
+		  wts.cash = 1 - wts.risky
+		  wts.efront = rbind(muvals,sigmavals,wts.cash,wts.risky)
+		  row.names(wts.efront) = c("MU","VOL","Cash","Risky Assets")
+		  print(wts.efront)
+		  
+		  if(values){
+		    return(wts.efront)
+		  }
 }
