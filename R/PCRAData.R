6a11cc99b2e8bd0b0c5f8af18a65b1b87df45c5a
Alexios Galanos <alexios@4dscape.com>
2022-11-23 18:03:56 -0800

    Added argument validation to getPCRAData

--- R/PCRAData.R
+++ R/PCRAData.R
@@ -34,6 +34,16 @@
 #'
 getPCRAData <- function(dataset = "crsp.returns8", cache = TRUE, refresh = FALSE)
 {
+  valid_datasets <- c("stocksCRSP","stocksCRSPdaily",
+                      "stocksCRSPweekly","crsp.returns8",
+                      "crsp.short","factorsSPGMI",
+                      "FRBrates1934to2014",
+                      "smallcapM","smallcapW",
+                      "largecapM","largecapW",
+                      "normalVsHectic", "ret4withOutliers",
+                      "strategies","WeeklyReturns1980",
+                      "wtsGmvLS","gfunds5","invensysEPS")
+  dataset <- match.arg(dataset[1], valid_datasets)
   base_url <- "https://github.com/robustport/PCRA/raw/main/DataPlus/"
   data_url <- paste(base_url,dataset,".rds", sep = "")
   key <- list(data_url)
