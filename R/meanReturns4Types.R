448fb218a7296d3883a8573a83ff0b1305a572d3
robustport <dougPCRA@gmail.com>
2022-11-29 20:10:59 -0800

    New function with new name for old function.

---
+++ R/meanReturns4Types.R
@@ -0,0 +1,35 @@
+#' Four types of returns means for reporting
+#' 
+#' Computes arithmetic mean, logarithmic mean, geometric mean and an approximate
+#' geometric mean.
+#'
+#' @param x An xts or numeric vector of return
+#' @param robust A logical value controlling whether a classical or robust sample
+#' mean and standard deviation is computed. Default is FALSE 
+#'
+#' @return fourMeans
+#' @export
+#'
+#' @examples
+#' args(meansReturns4Report)
+meansReturns4Report <- function(ret,robust = FALSE,eff = 0.95)
+{
+  if(robust == FALSE) {retMu <- mean(ret)} else
+  {x <- locScaleM(ret,eff = eff)
+  retMu <- x$mu
+  }
+  logret <- log(ret+1)
+  if(robust == FALSE) {logretMu <- mean(logret)} else
+  {x <- locScaleM(logret,eff = eff)
+  logretMu <- x$mu
+  }
+  g <- exp(logretMu)-1
+  if(robust == FALSE) {S_squared <- var(ret)} else
+  {scale <- scaleTau2(ret)
+  S_squared <- scale^2
+  }
+  gApprox <- retMu - S_squared/2
+  fourMeans <- c(retMu,logretMu,g,gApprox)
+  names(fourMeans) <- c("ArithMean","LogMean","GeomMean","ApproxGMean")
+  return(fourMeans)
+}
