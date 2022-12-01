952309cfcce56033dd477a4cace01c6b79d3c80f
robustport <dougPCRA@gmail.com>
2022-12-01 15:00:20 -0800

    Clean up turnOver function

--- R/turnover.R
+++ R/turnover.R
@@ -1,32 +1,32 @@
-#' Turnover
+#' Calculate Portfolio Turnover
 #' 
-#' turnover is the function to calculate a times series of n-1 turn-over
+#' turnOver is the function to calculate a times series of n-1 turn-over
 #' values for a times of n portfolio weight vectors, where the turnover
 #' from time i to time i+1 is the sum of the absolute differences 
 #' between the portfolio weights at time i and time i+1
 #' 
-#' @param weights 
+#' @param weights multivariate xts object of portfolio weights
 #'
 #' @return Time series of turnover values
 #' @export
 #'
 #' @examples
-#' args(turnover)
-turnover = function(weights){
-  dates = index(weights)
-  weights=coredata(weights)
-  n.asset=ncol(weights)
-  n.dates=nrow(weights)
-  if(n.dates<2){
-    print("Less than 2 balancing dates!")
+#' args(turnOver)
+turnOver <- function(weights){
+  dates <- index(weights)
+  weights <- coredata(weights)
+  n.asset <- ncol(weights)
+  n.dates <- nrow(weights)
+  if(n.dates <2){
+    print("Less than 2 rebalancing dates!")
     return()
   }
-  TurnOver=rep(0,n.dates-1)
+  TurnOver <- rep(0,n.dates-1)
   for(i in 1:length(TurnOver)){
-    TurnOver[i]=sum(abs(weights[i+1,]-weights[i,]))
+    TurnOver[i] <- sum(abs(weights[i+1,] - weights[i,]))
   }
-  dates=dates[-1]
-  res=zoo(TurnOver,order.by = dates)
-  res
+  dates <- dates[-1]
+  out <- zoo(TurnOver,order.by = dates)
+  out
 }
 
