da5b0f434f9019ebcd13f6ea11a7b9b02e1e09c3
robustport <dougPCRA@gmail.com>
2022-12-01 20:06:58 -0800

    Edit man page and clean up R formatting

--- R/mathEfrontCashRisky.R
+++ R/mathEfrontCashRisky.R
@@ -1,51 +1,49 @@
-#' mathEfrontCashRisky
+#' @title Math Efficient Frontier: Cash and Risky Assets
 #' 
-#' This function computes and plots a linear efficient frontier that is
+#' @Description This function computes and plots a linear efficient frontier that is
 #' a mix of a risk-free asset ("cash") and risky stocks (or other assets).
 #' It optionally returns the weights along the linear efficient frontier.
 #'
-#' @param returns Risky asset returns in the form of an xts object 
-#' @param rf A risk-free rate
-#' @param scalex A scale parameter for scaling the horizontal axis
-#' @param scaley A scale parameter for scaling the vertical axis
-#' @param stock.names Logical variable which if TRUE results in stock names being displayed 
-#' @param npoints Number of points at which to compute efront values and
-#' weights
+#' @param returns Risky asset returns multivariate xts object 
+#' @param rf A risk-free rate with default 0.003
+#' @param scalex Horizontal axis scale parameter with default 1.1
+#' @param scaley Vertical axis scale parameter with default 1.1
+#' @param stock.names Logical variable with default TRUE
+#' @param npoints Number of efficient frontier points with default 10
 #' @param plot.efront Logical variable which if TRUE results in a plot of
-#' @param values Logical whether to return values in output
-#' @param cexPoints A size parameter for points
-#' @param cexText A size parameter for text
-#' the linear efficient frontier and the stock positions
+#' @param values Logical variable for returning efront values with default FALSE
+#' @param cexPoints Numerical size parameter for points with default 0.8
+#' @param cexText Numerical size parameter for text with default 0.8
 #' @return  A plot of the linear efficient frontier or the weights along the
-#' efficient frontier.  Optionally output the mean and vol values
+#' efficient frontier.  Optionally output the mean and volatility values
 #' of the linear efficient frontier or its weights, or both.
 #' @export
 #'
 #' @examples
 #' args(mathEfrontCashRisky)
 mathEfrontCashRisky <-
-		function(returns, npoints = 10, rf = 0.005, plot.efront = TRUE,
-		    stock.names = TRUE, values = FALSE,scalex = 1.1, scaley = 1.1,
+		function(returns, npoints = 10, rf = 0.003, plot.efront = TRUE,
+		    stock.names = TRUE, values = FALSE, scalex = 1.1, scaley = 1.1,
 		    cexPoints = 0.8, cexText = 0.8)
 {
-		  V = var(returns)
-		  mu.stocks = apply(returns, 2, mean)
-		  sigma.stocks = apply(returns, 2, sd)
-		  mue = mu.stocks - rf
-		  a = solve(V, mue)                  # a = Vinv * mue
-		  b = as.numeric(t(mue) %*% a)       # b = muetp * Vinv* mue
-		  sr.opt = sqrt(as.numeric(b))
-		  bsr = sqrt(b)
-		  mu.max = scaley * max(mu.stocks)
-		  muvals = seq(0,mu.max, length.out = npoints)
-		  sigmavals = c(muvals/bsr)
-		  muvalse = seq(0,mu.max, length.out = npoints)
-		  muvals = rf + muvalse
+		  V <- var(returns)
+		  mu.stocks <- apply(returns, 2, mean)
+		  sigma.stocks <- apply(returns, 2, sd)
+		  mue <- mu.stocks - rf
+		  a <- solve(V, mue)                  # a = Vinv * mue
+		  b <- as.numeric(t(mue) %*% a)       # b = muetp * Vinv* mue
+		  sr.opt <- sqrt(as.numeric(b))
+		  bsr <- sqrt(b)
+		  mu.max <- scaley * max(mu.stocks)
+		  muvals <- seq(0,mu.max, length.out = npoints)
+		  sigmavals <- c(muvals/bsr)
+		  muvalse <- seq(0,mu.max, length.out = npoints)
+		  muvals <- rf + muvalse
 		  if(plot.efront)
 		  {
-		    x = sigmavals; y = muvals
-		    xlim = c(0,scalex*max(sigma.stocks,x))
-		    ylim = c(min(mu.stocks),max(muvals))
+		    x <- sigmavals; y = muvals
+		    xlim <- c(0,scalex*max(sigma.stocks, x))
+		    ylim <- c(min(mu.stocks),max(muvals))
 		    plot(x, y, type = "l", xaxs = "i", lwd = 2, xlim = xlim, ylim = ylim,
 		         xlab = "Volatility", ylab = "Mean Return")
 		    points(sigma.stocks, mu.stocks, pch = 20, cex = cexPoints)
@@ -55,16 +53,16 @@ mathEfrontCashRisky <-
 		    }
 		    x <- xlim[1] + .05 * (xlim[2] - xlim[1])
 		    y <- ylim[2] - .02 * (ylim[2] - ylim[1])
-		    text(x,y,paste("SR = ",round(sr.opt,2),sep = ""),pos = 4)
-		    y = y - .07*(ylim[2] - ylim[1])
-		    text(x,y,paste("RF= ",round(rf,3),sep = ""),pos = 4)
+		    text(x,y,paste("SR = ", round(sr.opt,2), sep = ""), pos = 4)
+		    y <- y - .07*(ylim[2] - ylim[1])
+		    text(x,y,paste("RF = ",round(rf,3),sep = ""), pos = 4)
 		  }
 		  
 		  # Compute cash and risky assets weights
-		  wts.risky = (sum(a)/b)*muvalse
-		  wts.cash = 1 - wts.risky
-		  wts.efront = rbind(muvals,sigmavals,wts.cash,wts.risky)
-		  row.names(wts.efront) = c("MU","VOL","Cash","Risky Assets")
+		  wts.risky <- (sum(a)/b)*muvalse
+		  wts.cash <- 1 - wts.risky
+		  wts.efront <- rbind(muvals, sigmavals, wts.cash, wts.risky)
+		  row.names(wts.efront) <- c("MU", "VOL", "Cash" ,"Risky Assets")
 		  
 		  if(values){
 		    return(wts.efront)
