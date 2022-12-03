d91107b783e938121808c62026b44d17d388e237
robustport <dougPCRA@gmail.com>
2022-12-02 19:51:14 -0800

    Simple man page and function edits.

--- R/KRest.R
+++ R/KRest.R
@@ -1,21 +1,21 @@
 #' @title Kurtosis Estimator
 #' 
 #' @description Sample estimate of excess kurtosis, with option for ordinary
-#' kurtosis. This function will eventually have a robust estimation option.
-#'
+#' kurtosis. This function will eventually have a robust estimator option.
+
 #' @param x A numeric vector
 #' @param excess A logical variable with default TRUE, which results in the
 #' computation of excess kurtosis, and FALSE results ordinary kurtosis.
 #'
-#' @return The value of excess kurtosis or ordinary kurtosis
+#' @return The estimate of excess kurtosis or ordinary kurtosis
 #'
 #' @examples
 #' args(KRest)
 #' @export
-KRest =  function(x,excess = TRUE)
+KRest <- function(x,excess = TRUE)
 {
-  n = length(x)
-  k = 1/n
+  n <- length(x)
+  k <- 1/n
   KR <- k*sum(((x-mean(x))/(sd(x)*sqrt((n-1)/n)))^4)
   if(excess){
     KR <- KR - 3} else
--- R/SKest.R
+++ R/SKest.R
@@ -1,19 +1,19 @@
-#' SKest
+#' @title Skewness estimator
 #' 
-#' Sample estimate of skewness
+#' @description Sample estimate of skewness
 #' This function will eventually have a robust estimation option
 #'
 #' @param x A numeric vector
 #'
-#' @return Value of sample skewness
+#' @return The estimate of skewness
 #' 
 #' @examples
 #' args(SKest)
 #' @export
-SKest =  function(x)
+SKest <- function(x)
 {
-  n = length(x)
-  k = 1/n
+  n <- length(x)
+  k <- 1/n
   SK <- k*sum(((x-mean(x))/(sd(x)*sqrt((n-1)/n)))^3)
   return(SK)
 }
