915468d72c4280b0cd00cc9a43cb484649cdfb27
robustport <dougPCRA@gmail.com>
2023-02-18 16:07:11 -0700

    Minor edits to man pages/

--- R/winsorMean.R
+++ R/winsorMean.R
@@ -5,27 +5,27 @@
 #' @param na.rm Logical variable with default FALSE
 #' @param ... Pass-through parameters
 #'
-#' @return Value of Winsorized mean
+#' @return Numeric value of Winsorized mean
 #' @export
 #'
 #' @examples
 #' args(winsorMean)
-winsorMean <- function(x, trim = 0, na.rm = FALSE, ...) {
+winsorMean <- function(x, winFrac = 0, na.rm = FALSE, ...) {
   if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
     warning("argument is not numeric or logical: returning NA")
     return(NA)
   }
   if (na.rm)
     x <- x[!is.na(x)]
-  if (!is.numeric(trim) || length(trim) != 1L)
-    stop("'trim' must be numeric of length one")
+  if (!is.numeric(winFrac) || length(winFrac) != 1L)
+    stop("'winFrac' must be numeric of length one")
   n <- length(x)
   if (trim > 0 && n) {
     if (is.complex(x))
       stop("trimmed means are not defined for complex data")
-    if (trim >= 0.5)
+    if (winFrac >= 0.5)
       return(median(x, na.rm = FALSE))
-    lo <- floor(n * trim) + 1
+    lo <- floor(n * winFrac) + 1
     hi <- n + 1 - lo
     x <- sort(x)
     if ( lo > 0 ) x[1:lo] <- x[lo]
