be31a11694d3b202a0616697975f5ee2acf5351a
Ke Li (Kirk) <cocokecoli@gmail.com>
2024-11-05 15:06:18 -0800

    updates on package installation from github

--- R/updateDevPkg.R
+++ R/updateDevPkg.R
@@ -5,7 +5,7 @@ getVersionLocal = function(pkg, field, lib.loc=NULL){
   if (nzchar(dcf)) read.dcf(dcf, fields=field)[1L] else NA_character_
 }
 # 
-getVersionGithub = function(repo, field, type) {
+getVersionGithub = function(repo, field) {
   # get DESCRIPTION metadata field from remote PACKAGES file
   stopifnot(is.character(field), length(field)==1L, is.character(repo), length(repo)==1L, field!="Package")
   idx = file("https://raw.githubusercontent.com/robustport/PCRA/refs/heads/main/DESCRIPTION")
@@ -13,9 +13,8 @@ getVersionGithub = function(repo, field, type) {
   read.dcf(idx, fields=field)
 }
 
-# repo="https://Rdatatable.gitlab.io/data.table"
 
-update_dev_pkg = function(pkg="PCRA", repo="https://github.com/robustport/PCRA",lib=NULL, ...) {
+update_dev_pkg = function(pkg="PCRA", repo="https://github.com/robustport/PCRA",field= "Version", lib=NULL) {
   # this works for any package, not just data.table
   # perform package upgrade when new Revision present
   stopifnot(is.character(pkg), length(pkg)==1L, !is.na(pkg),
@@ -23,17 +22,17 @@ update_dev_pkg = function(pkg="PCRA", repo="https://github.com/robustport/PCRA",
             is.character(field), length(field)==1L, !is.na(field),
             is.null(lib) || (is.character(lib) && length(lib)==1L && !is.na(lib)))
   # get Revision field from remote repository PACKAGES file
-  una = is.na(ups<-getVersionGithub(repo, field, type))
+  una = is.na(ups<-getVersionGithub(repo, field))
   if (una)
-    catf("No revision information found in DESCRIPTION file for %s package. Make sure that '%s' is correct field in PACKAGES file in your package repository '%s'. Otherwise package will be re-installed every time, proceeding to installation.\n",
-         pkg, field, contrib.url(repo, type=type))
+    catf("No revision information found in DESCRIPTION file for %s package. Make sure that '%s' is correct field in PACKAGES file in your package repository. Otherwise package will be re-installed every time, proceeding to installation.\n",
+         pkg, field)
   # see if Revision is different then currently installed Revision, note that installed package will have Revision info only when it was installed from remote devel repo
   upg = una || !identical(ups, getVersionLocal(pkg, field))
   # update_dev_pkg fails on windows R 4.0.0, we have to unload package namespace before installing new version #4403
   on.exit({
     if (upg) {
       unloadNamespace(pkg) ## hopefully will release dll lock on Windows
-      utils::install.packages(pkg, repos=repo, type=type, lib=lib), ...)
+      devtools::install_github(repo=repo)
       msg_fmt = gettext("R %s package has been updated to %s (%s)\n")
     } else {
       msg_fmt = gettext("R %s package is up-to-date at %s (%s)\n")
