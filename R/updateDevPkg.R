9cd1ec33e23f2c10f0c9165b5f6479dc075fe662
Ke Li (Kirk) <cocokecoli@gmail.com>
2024-11-05 15:28:30 -0800

    update

--- R/updateDevPkg.R
+++ R/updateDevPkg.R
@@ -5,6 +5,7 @@ getVersionLocal = function(pkg, field, lib.loc=NULL){
   if (nzchar(dcf)) read.dcf(dcf, fields=field)[1L] else NA_character_
 }
 # 
+
 getVersionGithub = function(repo, field) {
   # get DESCRIPTION metadata field from remote PACKAGES file
   stopifnot(is.character(field), length(field)==1L, is.character(repo), length(repo)==1L, field!="Package")
@@ -14,6 +15,17 @@ getVersionGithub = function(repo, field) {
 }
 
 
+#' Update to Developer version on Github
+#'
+#' @param pkg 
+#' @param repo 
+#' @param field 
+#' @param lib 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 update_dev_pkg = function(pkg="PCRA", repo="https://github.com/robustport/PCRA",field= "Version", lib=NULL) {
   # this works for any package, not just data.table
   # perform package upgrade when new Revision present
@@ -22,20 +34,20 @@ update_dev_pkg = function(pkg="PCRA", repo="https://github.com/robustport/PCRA",
             is.character(field), length(field)==1L, !is.na(field),
             is.null(lib) || (is.character(lib) && length(lib)==1L && !is.na(lib)))
   # get Revision field from remote repository PACKAGES file
-  una = is.na(ups<-getVersionGithub(repo, field))
+  una = is.na(ups<-.getVersionGithub(repo, field))
   if (una)
     catf("No revision information found in DESCRIPTION file for %s package. Make sure that '%s' is correct field in PACKAGES file in your package repository. Otherwise package will be re-installed every time, proceeding to installation.\n",
          pkg, field)
   # see if Revision is different then currently installed Revision, note that installed package will have Revision info only when it was installed from remote devel repo
-  upg = una || !identical(ups, getVersionLocal(pkg, field))
+  upg = una || !identical(ups,.getVersionLocal(pkg, field))
   # update_dev_pkg fails on windows R 4.0.0, we have to unload package namespace before installing new version #4403
   on.exit({
     if (upg) {
       unloadNamespace(pkg) ## hopefully will release dll lock on Windows
       devtools::install_github(repo=repo)
-      msg_fmt = gettext("R %s package has been updated to %s (%s)\n")
+      msg_fmt = gettext("R %s package has been updated to %s (from %s)\n")
     } else {
-      msg_fmt = gettext("R %s package is up-to-date at %s (%s)\n")
+      msg_fmt = gettext("R %s package is up-to-date at %s (from %s)\n")
     }
     field_val = unname(read.dcf(system.file("DESCRIPTION", package=pkg, lib.loc=lib, mustWork=TRUE), fields=field)[, field])
     cat(sprintf(msg_fmt, pkg, field_val, utils::packageVersion(pkg, lib.loc=lib)))
